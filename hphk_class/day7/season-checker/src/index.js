0d2ec65def04f4710c6481be7242d9e5fdd31eae
kihong <ganzik83@gmail.com>
2019-11-18 11:26:29 +0900

    [MOD] 클래스 컴포넌트

--- hphk_class/day7/season-checker/src/index.js
+++ hphk_class/day7/season-checker/src/index.js
@@ -3,3 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// life cycle method
+
+// constructor() 단 한번 실행되면 좋은 코드들을 담는다.
+
+// render() {return (jsx template)}
+
+// componentDidMount() {컴포넌트가 화면에 랜더링이 끝나면}
+
+// componentDidUpdate() {컴포넌트가 업데이트가 되고, 리랜더링이 끝나면}
+
+// componentWillUnmount() {React요소들이 아닌 친구들을 치울 때 좋다.}
